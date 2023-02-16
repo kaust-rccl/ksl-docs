f1e8b2381d97115231217807014e7d3adf12fcce
Mohsin A Shaikh <mohsinshaikh786@gmail.com>
2023-02-16 15:34:59 +0300

    Add hierarchical pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KAUST Supercomputing Lab Support Documentation'
+copyright = '2023, KSL'
+author = 'Mohsin Ahmed Shaikh'
 
 release = '0.1'
 version = '0.1.0'
---
+++ docs/source/systems/ibex/index.rst
@@ -0,0 +1,14 @@
+KSL HPC Systems
+===================================================
+
+Contents
+--------
+
+.. toctree::
+   shaheen2/index
+   shaheen3
+   ibex
+
+
+
+
